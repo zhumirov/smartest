f8045c483d2b18e0b246d891ce2cc61ee5c91f60
Bexultan <zhumirov@mail.ru>
2024-02-25 01:23:18 +0600

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+import Footer from "@/components/shared/Footer";
+import Header from "@/components/shared/Header";
+
+export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <div className="flex-h h-screen flex-col">
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+        
+      
+    );
+  }
+  
